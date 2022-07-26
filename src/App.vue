e4c4b5b054caac9589370f961e74282bf4b5bf4a
Lector <lector1982@gmail.com>
2022-07-26 18:26:18 +0100

    add transition and switch animations

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,39 @@
 		<router-link to="/contacts">Contacts</router-link>
 	</nav>
 
+	<div class="radios">
+		<label><input type="radio" name="anim" value="fade" v-model="anim" checked> Fade</label>
+		<label><input type="radio" name="anim" value="scale" v-model="anim"> Scale</label>
+		<label><input type="radio" name="anim" value="slide-left" v-model="anim"> Slide-left</label>
+		<label><input type="radio" name="anim" value="slide-right" v-model="anim"> Slide-right</label>
+		<label><input type="radio" name="anim" value="slide-down" v-model="anim"> Slide-down</label>
+	</div>
+
+<!-- <transition name="fade" appear mode="out-in">
 	<router-view />
+</transition> -->
+
+<router-view v-slot="{Component}">
+	<transition :name="anim" mode="out-in" appear>
+		<component :is="Component" />
+	</transition>
+</router-view>
 
 </template>
 
 <script>
 
+import {ref} from 'vue'
+
 export default {
   name: 'App',
+	setup() {
+		const anim = ref('fade');
+
+		return {
+			anim
+		}
+	}
 
 }
 </script>
@@ -30,13 +55,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 30px;
+  padding-top: 30px;
+	height: 100vh;
+	overflow: hidden;
+}
+main {
+	height: 100%;
+	overflow-y: auto;
 }
 nav {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	margin-bottom: 40px;
+	margin-bottom: 30px;
 }
 nav a {
 	margin: 0 10px;
@@ -63,5 +94,67 @@ img {
 	margin: 20px auto;
 	display: block;
 	height: auto;
+}
+.radios {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 40px;
+}
+label {
+	display: flex;
+	align-items: center;
+	margin: 0 20px;
+	font-size: 1.1rem;
+}
+
+/* animations */
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+	transition: .5s ease-in-out;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+	opacity: 0;
+	transform: scale(0.2);
+}
+.scale-enter-active,
+.scale-leave-active {
+	transition: .5s ease-in-out;
+}
+
+.slide-left-enter-from,
+.slide-left-leave-to {
+	opacity: 0;
+	transform: translateX(-100%);
+}
+.slide-left-enter-active,
+.slide-left-leave-active {
+	transition: .5s ease-in-out;
+}
+
+.slide-right-enter-from,
+.slide-right-leave-to {
+	opacity: 0;
+	transform: translateX(100%);
+}
+.slide-right-enter-active,
+.slide-right-leave-active {
+	transition: .5s ease-in-out;
+}
+
+.slide-down-enter-from,
+.slide-down-leave-to {
+	opacity: 0;
+	transform: translateY(100%);
+}
+.slide-down-enter-active,
+.slide-down-leave-active {
+	transition: .5s ease-in-out;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,5 +4,8 @@
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, necessitatibus. Obcaecati in suscipit dolorum consectetur nulla dignissimos labore nisi numquam natus dolore. Reprehenderit nihil atque consequuntur magnam quo eum sint!</p>
 		<img src="https://picsum.photos/800/300" alt="">
 		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti aut magni repellendus suscipit, obcaecati repellat perferendis ut, nisi quis nulla cumque architecto, debitis sed quia ad! Ipsum fuga dolores asperiores!</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla velit voluptate exercitationem beatae excepturi, vitae cupiditate illum. Iure, incidunt error sapiente illum velit id dolorum eum accusamus repellendus ex dolor?</p>
+		<img src="https://picsum.photos/800/300" alt="">
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non accusamus facere fugiat eveniet harum facilis possimus ipsum culpa tempore sit quo aliquam totam quis cupiditate quod maiores at, repudiandae voluptatem?</p>
 	</main>
 </template>
